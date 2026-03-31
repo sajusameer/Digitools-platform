fc2af60cbada5966e3297096144b316a016f3eb0
sajusameer <sajusameer32@gmail.com>
2026-03-31 19:08:35 +0600

    rating added

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -18,7 +18,7 @@ const Cart = ({ carts, setCarts }) => {
   };
 
   return (
-    <div className='max-w-4xl mx-auto p-6'>
+    <div className='max-w-6xl mx-auto p-6'>
 
       <h2 className='text-3xl font-bold mb-5'>Your Cart</h2>
 
